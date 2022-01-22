97c9e7ad0e480e0474fe165b1e24d574bbcad50a
Cristina <crisanguti@gmail.com>
2022-01-22 21:59:41 +0100

    Initial commit

---
+++ src/EjerciciounoSesionunoJavaBasico.java
@@ -0,0 +1,35 @@
+public class EjerciciounoSesionunoJavaBasico {
+
+    public static void main(String[] args) {
+        //Tipos de datos
+
+        // Numéricos
+            //Enteros
+        byte var1=127;
+        short var2=32767;
+        int var3=2147483647;
+        long var4=9223372036854775806L;
+            //Decimales
+        float var5=1.2f;
+        double var6=9.65d;
+        //Booleanos
+        boolean var7=true;
+        boolean var8=false;
+        //Texto
+        char var9='c';
+        String var10="Hakuna Matata";
+
+        //Imprimir los resultados en pantalla
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var3);
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+        System.out.println(var8);
+        System.out.println(var9);
+        System.out.println(var10);
+
+    }
+}
